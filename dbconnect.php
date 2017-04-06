da6f376eb5ecceecc66e56580f25a33b5c5d75a8
Lindsay Ward <lindsay.ward@jcu.edu.au>
2017-04-06 17:14:47 +1000

    Update link to image, remove test DB, update PHP formatting

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,10 @@
 <?php
 // error reporting - E_ALL is the highest, for during development
-error_reporting(E_ALL);
+error_reporting( E_ALL );
 session_start();
 try {
 	// connect to SQLite database 
-    $dbh = new PDO("sqlite:db/advice.sqlite"); 
+	$dbh = new PDO( "sqlite:db/advice.sqlite" );
+} catch ( PDOException $e ) {
+	echo $e->getMessage();
 }
-catch(PDOException $e)
-{
-    echo $e->getMessage();
-}
-?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include("dbconnect.php"); ?>
+<?php include( "dbconnect.php" ); ?>
 <!doctype html>
 <html>
 <head>
@@ -8,12 +8,13 @@
 </head>
 
 <body>
-<?php include("inc_header.php");
-include("inc_nav.php"); ?>
+<?php include( "inc_header.php" );
+include( "inc_nav.php" ); ?>
 <section id="content">
     <h2>Welcome to The Advice Shop </h2>
 
-    <p><strong>You need advice</strong>. It's impossible these days to really succeed on your own with the complexity and fast pace of
+    <p><strong>You need advice</strong>. It's impossible these days to really succeed on your own with the complexity
+        and fast pace of
         the modern world. Let us help you go beyond your current limits and experience the next level of success!</p>
 
     <p>We specialise in providing advice. We've got opinions, tips, suggestions and all kinds of advice for any
@@ -21,7 +22,7 @@ include("inc_nav.php"); ?>
 
     <p><a href="subscribe.php">Subscribe now</a> to our professional advice service.</p>
 
-    <p>For all advice related to:<img src="../theadviceshop/images/womanHeadset.jpg" alt="" width="310" height="200"
+    <p>For all advice related to:<img src="images/womanHeadset.jpg" alt="" width="310" height="200"
                                       class="rightImage"/></p>
     <ul>
         <li>Learning</li>
@@ -30,10 +31,10 @@ include("inc_nav.php"); ?>
         <li>Language</li>
         <li>and anything else...</li>
     </ul>
-    <p>Services</p>
+    <h3>Services</h3>
 
     <p>We have great things.</p>
 </section>
-<?php include("inc_footer.php"); ?>
+<?php include( "inc_footer.php" ); ?>
 </body>
 </html>
