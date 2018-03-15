736bef4d6029fbc3def94e3e1fc3e93d9cee76ca
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-03-15 12:23:52 +1000

    Update main icon and home page text

--- inc_header.php
+++ inc_header.php
@@ -3,5 +3,5 @@ $filename = substr(strrchr($_SERVER['SCRIPT_NAME'], "/"), 1); // missing '' arou
 $name = substr($filename, 0, strrpos($filename, ".")); ?>
 <header id="pageHeader">
   <aside id="login"><form action="login.php?page=<?php echo $name; ?>" method="post"><input name="username" id="username" type="text" placeholder="Username"><input name="submit" type="submit" value="Login"></form></aside>
-<a href="index.php"><h1><span class="glyphicon glyphicon-bullhorn"></span> The Advice Shop</h1></a>
+<a href="index.php"><h1><span class="glyphicon glyphicon-ok"></span> The Advice Shop</h1></a>
 </header>
