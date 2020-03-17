26b545231456ea7648c299bb66719d7dfeb38987
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-03-17 11:48:33 +1000

    Update subscription claim

--- subscribe.php
+++ subscribe.php
@@ -10,7 +10,8 @@
 <body>
 <?php include("inc_header.php");
 include("inc_nav.php"); ?>
-<h2>Subscribe now for great advice!!!</h2>
+<h2>Subscribe now for great advice!</h2>
+<h3>We are so confident, we offer a mone-back guarantee :)</h3>
 <div id="content">
     <form action="process.php" method="get">
   <p>
